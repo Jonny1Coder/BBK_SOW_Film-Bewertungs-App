c27dc52b6d750024eeb4dbb95a781d2dccad19fe
Jonathan <jonathan.uloth@gmail.com>
2026-03-11 08:42:04 +0100

    Add navigation component and update Home layout with localized title

---
+++ src/lib/components/Layout/Nav.svelte
@@ -0,0 +1,13 @@
+<script>
+    
+</script>
+
+<nav>
+    <a href="./">Home</a>
+    <a href="/filme">Alle Filme</a>
+    <a href="/cart">Warenkorb</a>
+</nav>
+
+<style>
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Nav from "../lib/components/Layout/Nav.svelte";
+</script>
+<Nav />
+<main>
+    <h1>Willkommen</h1>
+</main>
+
